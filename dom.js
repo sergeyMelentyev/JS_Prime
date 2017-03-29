07cb7fcd3af3b888ad2b130d27105c21824e131f
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-03-29 15:08:59 +0300

    Update dom.js

--- dom.js
+++ dom.js
@@ -5,12 +5,15 @@ var elem = document.querySelector('div p');
 var elem = document.querySelector("[data-role='p']");
 
 
+
 /* COLLECTIONS */
 // html collection, methods .length and .item(index)
 var elem = document.getElementsByTagName('p');      // life collection
 // node elements collection, methods .length and .item(index)
 var elem = document.querySelectorAll('div p');
+var elem = document.querySelectorAll('*[class="name"]');    // any elems with same class name
 var array = [].slice.call(elem);        // get array from NodeList object
+
 
 
 /* GET ELEMENTS */
@@ -23,22 +26,41 @@ if (elem.hasChildNodes()) {
 }
 
 
+
 /* GET AND SET ATTRIBUTES */
 var atr = document.getElementById('one').getAttribute("data-role");
 elem.setAttribute("data-role", atr);
 
 
-/* CREATE ELEMENT */
+
+/* CREATE AND ADD NODE ELEMENT OR TEXT NODE */
 var p = document.createElement('p');
 var span = document.createElement('span'); span.innerText = 'text';
 p.appendChild(span);
 var elements = document.getElementsByTagName('p');
 elements[0].parentNode.appendChild(p);
 
+var div = document.getElementById('id');    // get the target 'div'
+var elems = div.getElementsByTagName('p');  // retrieve a collection from 'div'
+var newElem = document.createElement('span');
+div.insertBefore(newElem, elems[3]);
+
+var text = document.createTextNode("text");
+var p = document.createElement('p').appendChild(text);
+
+
+
+/* DELETE NODE ELEMENT */
+var one = document.getElementById('one');
+var set = one.getElementsByTagName('p');
+one.removeChild(set[0]);
+
+
 
 /* ADD CLASS IDENTIFIER */
-var name = "name";
-document.body.classList.add(name);
+elem.setAttribute('class', 'class-name');
+elem.classList.add('class-name');       // .remove
+
 
 
 /* ADD CALLBACKS TO ELEMS */
@@ -53,4 +75,10 @@ function namedFunc(item) {      // add event listener for each object
         item.textContent = "any text";      // set any content text
     });
 }
+
+
+
+/* STYLES */
+elem.style.backgroundColor = 'red';
+elem.setAttribute('style', 'background-color: red; color: white;');
 
