3771f3e23f67da32a29706c2ca632254dc1cf90f
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-03-29 17:37:59 +0300

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,84 +1,109 @@
-
-/* SELECTORS */
-var elem = document.getElementById('id');
-var elem = document.querySelector('div p');
-var elem = document.querySelector("[data-role='p']");
-
-
-
-/* COLLECTIONS */
-// html collection, methods .length and .item(index)
-var elem = document.getElementsByTagName('p');      // life collection
-// node elements collection, methods .length and .item(index)
-var elem = document.querySelectorAll('div p');
-var elem = document.querySelectorAll('*[class="name"]');    // any elems with same class name
-var array = [].slice.call(elem);        // get array from NodeList object
+/* SINGLE ELEM SELECTOR */
+(function () {
+    var elem = document.getElementById('id');
+    var elem = document.querySelector('div p');
+    var elem = document.querySelector("[data-role='p']");
+})();
 
 
+/* COLLECTIONS SELECTOR */
+(function () {
+    // html collection, methods .length and .item(index)
+    var elem = document.getElementsByTagName('p');      // life collection
+    // node elements collection, methods .length and .item(index)
+    var elem = document.querySelectorAll('div p');
+    var elem = document.querySelectorAll('*[class="name"]');    // any elems with same class name
+    var array = [].slice.call(elem);        // get array from NodeList object
+})();
 
-/* GET ELEMENTS */
-var elem = document.getElementById('id');
-var childNodesName = [];
-if (elem.hasChildNodes()) {
-    var children = elem.childNodes;      // .parentNode
-    for (var i = 0; i < children.length; i++)
-        childNodesName.push(children[i].nodeName);
-}
 
+/* GET NODE ELEMENTS */
+(function () {
+    var elem = document.getElementById('id');
+    var childNodesName = [];
+    if (elem.hasChildNodes()) {
+        var children = elem.childNodes;      // .parentNode
+        for (var i = 0; i < children.length; i++)
+            childNodesName.push(children[i].nodeName);
+    }
+})();
 
 
 /* GET AND SET ATTRIBUTES */
-var atr = document.getElementById('one').getAttribute("data-role");
-elem.setAttribute("data-role", atr);
+(function () {
+    var atr = document.getElementById('one').getAttribute("data-role");
+    atr.setAttribute("data-role", atr);
+})();
 
 
+/* CREATE NODE ELEMENT OR TEXT NODE */
+(function () {
+    var p = document.createElement('p');
+    var span = document.createElement('span'); span.innerText = 'text';
+    p.appendChild(span);
+    var elements = document.getElementsByTagName('p');
+    elements[0].parentNode.appendChild(p);
 
-/* CREATE AND ADD NODE ELEMENT OR TEXT NODE */
-var p = document.createElement('p');
-var span = document.createElement('span'); span.innerText = 'text';
-p.appendChild(span);
-var elements = document.getElementsByTagName('p');
-elements[0].parentNode.appendChild(p);
+    var div = document.getElementById('id');    // get the target 'div'
+    var elems = div.getElementsByTagName('p');  // retrieve a collection from 'div'
+    var newElem = document.createElement('span');
+    div.insertBefore(newElem, elems[3]);
 
-var div = document.getElementById('id');    // get the target 'div'
-var elems = div.getElementsByTagName('p');  // retrieve a collection from 'div'
-var newElem = document.createElement('span');
-div.insertBefore(newElem, elems[3]);
-
-var text = document.createTextNode("text");
-var p = document.createElement('p').appendChild(text);
-
+    var text = document.createTextNode("text");
+    var p = document.createElement('p').appendChild(text);
+})();
 
 
 /* DELETE NODE ELEMENT */
-var one = document.getElementById('one');
-var set = one.getElementsByTagName('p');
-one.removeChild(set[0]);
-
+(function () {
+    var one = document.getElementById('one');
+    var set = one.getElementsByTagName('p');
+    one.removeChild(set[0]);
+})();
 
 
 /* ADD CLASS IDENTIFIER */
-elem.setAttribute('class', 'class-name');
-elem.classList.add('class-name');       // .remove
+(function () {
+    elem.setAttribute('class', 'class-name');
+    elem.classList.add('class-name');       // .remove
+})();
 
 
-
-/* ADD CALLBACKS TO ELEMS */
-var elem = document.querySelectorAll('div p');
-var array = [].slice.call(elem);
-array.forEach(namedFunc);        // pass each obj from array to namedFunc as an arg
-function namedFunc(item) {      // add event listener for each object
-    item.addEventListener("click", function (event) {
-        event.preventDefault();
-        var atr = item.getAttribute("data-image-url");    // get any attribute
-        item.setAttribute("src", atr);      // set any attribute
-        item.textContent = "any text";      // set any content text
-    });
-}
-
+/* ADD CALLBACKS TO ARRAY OF ELEMS */
+(function () {
+    var elem = document.querySelectorAll('div p');
+    var array = [].slice.call(elem);
+    array.forEach(namedFunc);        // pass each obj from array to namedFunc as an arg
+    function namedFunc(item) {      // add event listener for each object
+        item.addEventListener("click", function (event) {
+            event.preventDefault();
+            var atr = item.getAttribute("data-image-url");    // get any attribute
+            item.setAttribute("src", atr);      // set any attribute
+            item.textContent = "any text";      // set any content text
+        });
+    }
+})();
 
 
 /* STYLES */
-elem.style.backgroundColor = 'red';
-elem.setAttribute('style', 'background-color: red; color: white;');
+(function () {
+    elem.style.backgroundColor = 'red';
+    elem.setAttribute('style', 'background-color: red; color: white;');
+})();
+
+
+/* POPUP OVERLAY */
+(function (global) {
+    function displayPopup() {
+        var overlay = document.createElement('div').setAttribute('id', 'overlay');
+        document.body.appendChild(overlay);
+        overlay.onclick = restore;
+    }
+    function restore() {
+        document.body.removeChild(document.getElementById('overlay'));
+    }
+    global.onload = function () {
+        displayPopup();
+    };
+})(window);
 
