4f4a066d7a48aff7b31a98fa11e571dc626c965f
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-07-13 17:58:15 +0300

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
-function name() {
-	
+function global() {
+	global;						// global object
+	process;					// shell environment, "process.env" get environment data
 }
 function debug() {
-	// :> node debug name.js			// help, list(10), setBreakpoint(line), repl
+	// :> node debug name.js	// help, list(10), setBreakpoint(line), repl
 }
